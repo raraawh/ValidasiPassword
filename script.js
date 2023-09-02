fec11c952595c2bbe47533dc5d044811e61254bc
= <=>
2023-09-02 11:32:09 +0700

    first commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const inputPassword = document.getElementById('input-password');
 const errorMessage = document.getElementById('error-message');
 const submitButton = document.getElementById('submit-button');
 
-submitButton.addEventListener("click" ,() => {
+submitButton.addEventListener("click" , () => {
     const password = inputPassword.value;
     if(password.length> 7){
         errorMessage.textContent = password;
