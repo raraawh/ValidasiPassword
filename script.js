342b7759cd548148a87a0802b72ca46ad064467c
= <=>
2023-09-02 10:49:31 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const inputPassword = document.getElementById('input-password');
+const errorMessage = document.getElementById('error-message');
+const submitButton = document.getElementById('submit-button');
+
+submitButton.addEventListener("click" ,() => {
+    const password = inputPassword.value;
+    if(password.length> 7){
+        errorMessage.textContent = password;
+        errorMessage.style.color = "green";
+    } else {
+        errorMessage.textContent ="password terlalu pendek";
+        errorMessage.style.color = "red";
+    }
+});
